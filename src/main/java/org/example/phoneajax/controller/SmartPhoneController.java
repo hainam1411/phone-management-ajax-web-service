4be9e03b6c7abef445a13e2df7078a1933cc3c8d
hainam1411 <hainam141199@gmail.com>
2024-06-27 15:31:28 +0700

    add controller

--- src/main/java/org/example/phoneajax/controller/SmartPhoneController.java
+++ src/main/java/org/example/phoneajax/controller/SmartPhoneController.java
@@ -1,4 +1,42 @@
 package org.example.phoneajax.controller;
 
+import org.example.phoneajax.model.SmartPhone;
+import org.example.phoneajax.service.ISmartPhoneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Controller
+@CrossOrigin("*")
+@RequestMapping("api/smartphones")
 public class SmartPhoneController {
+private final ISmartPhoneService smartPhoneService;
+    @Autowired
+    public SmartPhoneController(ISmartPhoneService smartPhoneService) {
+        this.smartPhoneService = smartPhoneService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<SmartPhone>> listSmartPhone(){
+                  return new ResponseEntity<>(smartPhoneService.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<SmartPhone> createSmartphone(SmartPhone smartPhone){
+        return new ResponseEntity<>(smartPhoneService.save(smartPhone), HttpStatus.CREATED);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<SmartPhone> deleteSmartPhone(@PathVariable Long id){
+        Optional<SmartPhone> smartPhoneOptional = smartPhoneService.findById(id);
+        if(!smartPhoneOptional.isPresent()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+            smartPhoneService.remove(id);
+            return new ResponseEntity<>(smartPhoneOptional.get(), HttpStatus.NO_CONTENT);
+    }
+
 }
